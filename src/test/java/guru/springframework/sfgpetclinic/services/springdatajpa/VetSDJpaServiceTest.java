9f61b2ef2982f5d44d7c7333463d4bf5ba4486bf
vgtoui <toni.uimonen@tuni.fi>
2023-11-30 13:52:44 +0200

    Create VisitSDJpaServiceTest

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+
+    @Mock
+    VetRepository vetRepository;
+
+    @InjectMocks
+    VetSDJpaService vetSDJpaService;
+
+    @Test
+    void deleteById() {
+        vetSDJpaService.deleteById(1l);
+
+        verify(vetRepository).deleteById(1l);
+    }
+}
